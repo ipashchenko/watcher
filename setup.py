0bbaf3832392043a215b85cfc54359dfac1246ad
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-01-13 15:33:01 +0300

    adding script

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=open('README.rst').read(),
 
     packages=setuptools.find_packages(),
-    scripts=['bin/cron_command.py'],
+    scripts=['bin/cron_command.py', 'bin/add_cron_job.py'],
 
     install_requires=['python-crontab'],
 
@@ -20,9 +20,6 @@ setuptools.setup(
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7'
     ],
 )
